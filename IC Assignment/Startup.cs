68f147f90e61bfab38b8492126fdc32a2ae93180
Pei <jzha471@aucklanduni.ac.nz>
2017-08-20 23:01:08 +1200

    Add project files.

---
+++ IC Assignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IC_Assignment.Startup))]
+namespace IC_Assignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
